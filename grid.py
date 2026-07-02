3651bd322063a8f8e6d9ce2851ef1e393c06b989
cgalabrador128 <catherinegracelabrador@gmail.com>
2026-07-02 16:31:46 +0800

    passenger logic done, simulation inprogress

--- grid.py
+++ grid.py
@@ -1,28 +1,75 @@
 from tkinter import *
+#import passenger as p
 
 center=Tk()
-center.title("9x9 grid")
+center.title("15x6 grid")
 color = ''
+width = 50
+height = 50
 cells = {}
+cell_types = {}
 for row in range(15):
      for col in range(6):
         #color dictator
         if row == 0 and col == 0:
-           color = 'indianred3'
+           color, space_type = 'indianred3', 'red'
         elif (row < 7 and col == 1) or (row == 1 and col == 0) or (row > 1 and row < 8 and col==4) or ((row < 2 or (row>6 and row<10))and col==5) or (row==14 and col ==3):
-            color ='mediumpurple3'
+            color, space_type ='mediumpurple3', 'purple'
         elif (row<14 and (col>1 and col<4)) or ((row < 2 or (row > 7 and row < 10 )) and col == 4):
-            color = 'darkseagreen3'
+            color, space_type = 'darkseagreen3', 'green'
         elif ((row>1 and row < 7) and (col == 0 or col == 5)) or ((row>9) and ((col < 2 or col>3) or (col == 2 and row ==14))):
-            color = 'moccasin'
+            color, space_type = 'moccasin', 'yellow'
             #color='mediumpurple3'
         elif ((row > 6 and row < 10) and (col < 2)):
-            color = 'steelblue2'
+            color, space_type = 'steelblue2', 'blue'
 
-        cell = Frame(center, bg='white', highlightbackground="black",
+        cell = Canvas(center, bg='white', highlightbackground="black",
                      highlightcolor="black", highlightthickness=1, background = color,
-                     width=50, height=50,  padx=3,  pady=3)
+                     width=width, height=height)
         cell.grid(row=row, column=col)
         cells[(row, col)] = cell
+        cell_types[(row, col)] = space_type
 
+def is_valid_move(current_coords, next_coords):
+    nxt_row, nxt_col = next_coords
+    if nxt_row == 14:
+        return cell_types.get(next_coords)
+    return cell_types.get(next_coords) == 'green'
+
+def move_to_next_cell( curr, dest):
+    cells[curr].delete("all")
+    cells[dest].create_oval(5, 5, 45, 45, fill='white')
+    center.after(1000, circle_move_to_cell, dest)
+
+def circle_move_to_cell( curr = (8,4), dest = (14, 0)):
+    curr_row, curr_col = curr
+    dest_row, dest_col = dest
+    next_step = None
+    if curr_row == 0 or curr_row == dest_row:
+        step_dir = 1 if dest_col > curr_col else -1
+        next_step = (curr_row, curr_col + step_dir)
+    else:
+
+        step_dir = 1 if dest_row > curr_row else -1
+        test_step = (curr_row + step_dir, curr_col)
+        
+     
+        if is_valid_move(curr, test_step):
+            next_step = test_step
+        else:
+            
+            for col_offset in [-1, 1]:
+                alt_step = (curr_row, curr_col + col_offset)
+                if is_valid_move(curr, alt_step):
+                    next_step = alt_step
+                    break
+
+
+    if next_step and next_step in cells:
+        move_to_next_cell( curr, next_step)
+
+    
+cells[(8,4)].create_oval(5, 5, 45, 45, fill='white')
+center.after(100, circle_move_to_cell)
 center.mainloop()
+
--- passenger.py
+++ passenger.py
@@ -1,7 +1,192 @@
-passengers =[]
+#import grid as g
+import copy
 
+passengers = [3, 5, 6, 4, 9, 9, 5, 9, 8, 5, 4, 1, 8, 5, 6, 6, 8, 10, 10, 9, 4, 5, 5, 7, 8, 2, 
+              10, 8, 10, 4, 4, 1, 6, 1, 7, 2, 2, 6, 7, 6, 3, 5, 7, 9, 9, 9, 8, 10, 9, 4]
+p = [('p', val) for val in passengers[:6]]
+r = [('r', val) for val in passengers[6:]]
+passen = p + r
 
-def simulation(passengers):
+rows, cols = 15, 6
+row = [0] * cols
+res = [copy.deepcopy(row) for _ in range(rows)]
+
+# entrances passengers will come out from
+ent = [(0,4), (1,4), (8, 4), (9,4)]
+prio_seats = [(7, 0), (7,1), (8,0),(8,1), (9, 0), (9, 1)]
+seats = [(2,0), (2, 5), (3,0), (3,5), (4,0), (4,5), (5, 0), (5,5), (6, 0), (6, 5),
+        (10, 0), (10, 1), (10, 4), (10, 5),
+        (11, 0), (11, 1), (11, 4), (11, 5),
+        (12, 0), (12, 1), (12, 4), (12, 5),
+        (13, 0), (13, 1), (13, 4), (13, 5),
+    
+        (14, 0), (14, 1), (14, 2), (14, 4), (14, 5)]
+
+stand = [
+    (0, 2), (0, 3), (0, 4),
+    (1, 2), (1, 3), (1, 4),
+    
+    (2, 2), (2, 3),
+    (3, 2), (3, 3),
+    (4, 2), (4, 3),
+    (5, 2), (5, 3),
+    (6, 2), (6, 3),
+    
+    (7, 2), (7, 3),
+    
+    (8, 2), (8, 3), (8, 4),
+    (9, 2), (9, 3), (9, 4),
+    
+    (10, 2), (10, 3),
+    (11, 2), (11, 3),
+    (12, 2), (12, 3),
+    (13, 2), (13, 3),
+    
+    (14, 2)]
+
+#starting board is (8, 4)
+#max distance is 10
+def simulation(passengers = passengers):
     # passengers is the # on people to be boarded
     # prio is the # of priority passengers in the list of passengers
+    for r in passengers:
+
+        if r[0] == 'p':
+            max_dist = [0, [0,0]]
+            min_dist = [99999, [0,0]]
+            for i in prio_seats:
+                for e in ent:
+                    x = get_seat(e, i)
+                    if max_dist[0] < x:
+                        max_dist[0] = x
+                        max_dist[1] = i
+                    elif min_dist[0] > x:
+                        min_dist[0] = x
+                        min_dist[1] = i
+            if r[1] <= 5:
+                res[min_dist[1][0]][min_dist[1][1]] = r
+                x = prio_seats.index((min_dist[1][0],min_dist[1][1]))
+                prio_seats.pop(x)
+            else:
+                res[max_dist[1][0]][max_dist[1][1]] = r
+                x = prio_seats.index((max_dist[1][0],max_dist[1][1]))
+                prio_seats.pop(x)
+            #print(res, prio_seats)
+        else:
+            max_dist = [0, [0,0]]
+            min_dist = [99999, [0,0]]
+            #print('pass')
+            if seats:
+                for i in seats:
+                    for e in ent:
+                        x = get_seat(e, i)
+                        if max_dist[0] < x:
+                            max_dist[0] = x
+                            max_dist[1] = i
+                        elif min_dist[0] > x:
+                            min_dist[0] = x
+                            min_dist[1] = i
+                if r[1] <= 5:
+                    res[min_dist[1][0]][min_dist[1][1]] = r
+                    x = seats.index((min_dist[1][0],min_dist[1][1]))
+                    seats.pop(x)
+                else:
+                    res[max_dist[1][0]][max_dist[1][1]] = r
+                    x = seats.index((max_dist[1][0],max_dist[1][1]))
+                    seats.pop(x)
+                #print(res)
+            else: 
+                for i in stand:
+                    for e in ent:
+                        x = get_seat(e, i)
+                        if max_dist[0] < x:
+                            max_dist[0] = x
+                            max_dist[1] = i
+                        elif min_dist[0] > x:
+                            min_dist[0] = x
+                            min_dist[1] = i
+                if r[1] <= 5:
+                    res[min_dist[1][0]][min_dist[1][1]] = r
+                    x = stand.index((min_dist[1][0],min_dist[1][1]))
+                    stand.pop(x)
+                else:
+                    res[max_dist[1][0]][max_dist[1][1]] = r
+                    x = stand.index((max_dist[1][0],max_dist[1][1]))
+                    stand.pop(x)
     print('passengers')
+
+
+def rawfunc(passengers = passen, res = res):
+    # working with a 15 x 9 matrix grid
+    # r x c
+    # entracnes are (8,4), (9, 4), (0,4), (1,4)
+    for r in passengers:
+        if r[0] == 'p':
+            max_dist = [0, [0,0]]
+            min_dist = [99999, [0,0]]
+            for i in prio_seats:
+                for e in ent:
+                    x = get_seat(e, i)
+                    if max_dist[0] < x:
+                        max_dist[0] = x
+                        max_dist[1] = i
+                    elif min_dist[0] > x:
+                        min_dist[0] = x
+                        min_dist[1] = i
+            if r[1] <= 5:
+                x = prio_seats.index((min_dist[1][0],min_dist[1][1]))
+                prio_seats.pop(x)
+            else:
+                x = prio_seats.index((max_dist[1][0],max_dist[1][1]))
+                prio_seats.pop(x)
+            #print(res, prio_seats)
+        else:
+            max_dist = [0, [0,0]]
+            min_dist = [99999, [0,0]]
+            #print('pass')
+            if seats:
+                for i in seats:
+                    for e in ent:
+                        x = get_seat(e, i)
+                        if max_dist[0] < x:
+                            max_dist[0] = x
+                            max_dist[1] = i
+                        elif min_dist[0] > x:
+                            min_dist[0] = x
+                            min_dist[1] = i
+                if r[1] <= 5:
+                    x = seats.index((min_dist[1][0],min_dist[1][1]))
+                    seats.pop(x)
+                else:
+                    x = seats.index((max_dist[1][0],max_dist[1][1]))
+                    seats.pop(x)
+                #print(res)
+            else: 
+                for i in stand:
+                    for e in ent:
+                        x = get_seat(e, i)
+                        if max_dist[0] < x:
+                            max_dist[0] = x
+                            max_dist[1] = i
+                        elif min_dist[0] > x:
+                            min_dist[0] = x
+                            min_dist[1] = i
+                if r[1] <= 5:
+                    res[min_dist[1][0]][min_dist[1][1]] = r
+                    x = stand.index((min_dist[1][0],min_dist[1][1]))
+                    stand.pop(x)
+                else:
+                    res[max_dist[1][0]][max_dist[1][1]] = r
+                    x = stand.index((max_dist[1][0],max_dist[1][1]))
+                    stand.pop(x)
+    print(res)
+                
+
+
+def get_seat(pos, seatpos):
+    return get_dist(pos[0], pos[1], seatpos[0], seatpos[1])
+
+#Manhattan distance
+def get_dist(x1, x2, y1, y2):
+    return abs(x1 - x2) + abs(y1 - y2)
+rawfunc()
